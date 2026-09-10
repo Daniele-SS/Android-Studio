85223843cb1e651c43101ad6fb2b23faa534e58e
DANIELE SILVA SANTOS <25203622@sn1271249388.local>
2026-09-10 10:20:45 -0300

    feat: adiciona o .padding junto ao .background

--- app/src/main/java/com/example/composelayout/MainActivity.kt
+++ app/src/main/java/com/example/composelayout/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,7 +12,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composelayout.ui.theme.ComposeLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +24,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeLayoutTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(){
+
+                    Box(modifier = Modifier
+                        .padding(innerPadding) //ajusta o espaçamento do text
+                        .background(Color.Cyan) //adiciona cor no text
+                    ){
                         Text("Aula Android")
                     }
                 }
